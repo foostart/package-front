243a51596e18746c540068fbc779a0cd044c7133
foostart.com <foostart.com@gmail.com>
2019-03-17 12:04:59 +0700

    test

---
+++ Controllers/DetailController.php
@@ -0,0 +1,70 @@
+<?php namespace Foostart\Front\Controllers;
+
+/*
+|-------------------------------------------------------------------------------
+| HomeController
+|-------------------------------------------------------------------------------
+| @author: Kang
+| @website: http://foostart.com
+| @date: 27/01/2018
+|
+*/
+
+use Illuminate\Http\Request;
+use URL, Route, Redirect;
+use Illuminate\Support\Facades\App;
+
+use Foostart\Front\Controllers\FrontController;
+
+//Models
+use Foostart\Post\Models\Post;
+use Foostart\Slideshow\Models\Slideshow;
+
+class DetailController extends FrontController {
+
+    public $obj_post = NULL;
+    public $obj_slideshow = NULL;
+
+    public function __construct() {
+
+        parent::__construct();
+
+        //object item
+        $this->obj_post = new Post();
+        $this->obj_slideshow = new Slideshow();
+    }
+
+    /**
+     * Home page
+     */
+    public function index(Request $request) {
+
+        //init
+        $user = $this->getUser();
+
+        //Load list of categories
+        $params = array(
+            'order' => array(
+                'category_order' => 'ASC',
+            )
+        );
+        $course_categories = $this->obj_category->getCategoriesByIdParent('1', $params);//TODO: move 1 to config
+        $courses = $this->obj_post->getCoursesByCategoriesRoot($course_categories);
+
+        //load slid eshow author and customer
+        $slideshow_author_customer = $this->obj_slideshow->getSlideshowById('1');//TODO: move 1 to config
+
+        // display view
+        $this->data_view = array_merge($this->data_view, array(
+            'request' => $request,
+            'assets' => $this->getAssetPage('home'),
+            'title' => trans('front-user.pages.title-home'),
+            'courses' => $courses,
+            'slideshow_author_customer' => $slideshow_author_customer,
+            'user' => $user
+        ));
+
+        return view($this->package_name.'::pages.home', $this->data_view);
+    }
+
+}
--- routes.php
+++ routes.php
@@ -71,175 +71,20 @@ Route::group(['middleware' => ['web']], function () {
             'as' => 'home',
             'uses' => 'HomeController@index'
         ]);
-
         /**
-         * About
+         * Detail
          */
-        Route::get('/gioi-thieu', [
-            'as' => 'about',
-            'uses' => 'AboutController@index'
+        Route::get('/detail', [
+            'as' => 'detail',
+            'uses' => 'DetailController@index'
         ]);
-
         /**
-         * Blog
+         * Checklist
          */
-        Route::get('/blog', [
-            'as' => 'blog',
-            'uses' => 'BlogController@index'
+        Route::get('/checklist', [
+            'as' => 'checklist',
+            'uses' => 'DetailController@index'
         ]);
-
-        /**
-         * Blog detail
-         */
-        Route::get('/blog/{blog-detail}', [
-            'as' => 'blog-detail',
-            'uses' => 'BlogDetailController@index'
-        ])->where(['blog-detail' => '[a-zA-Z0-9-_]+']);
-
-        /**
-         * Contact
-         */
-        Route::get('/lien-he', [
-            'as' => 'contact',
-            'uses' => 'ContactController@index'
-        ]);
-
-        /**
-         * Edit profile
-         */
-        Route::get('/edit-profile', [
-            'as' => 'edit-profile',
-            'uses' => 'EditProfileController@index'
-        ]);
-
-        /**
-         * Error
-         */
-        Route::get('/error', [
-            'as' => 'error',
-            'uses' => 'ErrorController@index'
-        ]);
-
-        /**
-         * Event
-         */
-        Route::get('/su-kien', [
-            'as' => 'event',
-            'uses' => 'EventController@index'
-        ]);
-
-        /**
-         * Event detail
-         */
-        Route::get('/su-kien/{event}', [
-            'as' => 'event-detail',
-            'uses' => 'EventDetailController@index'
-        ])->where(['event' => '[a-zA-Z0-9-_]+']);
-
-        /**
-         * Faq
-         */
-        Route::get('/hoi-dap', [
-            'as' => 'faq',
-            'uses' => 'FaqController@index'
-        ]);
-
-        /**
-         * Gallery
-         */
-        Route::get('/gallery', [
-            'as' => 'gallery',
-            'uses' => 'GalleryController@index'
-        ]);
-
-        /**
-         * Lesson
-         */
-        Route::get('/khoa-hoc', [
-            'as' => 'lesson',
-            'uses' => 'LessonController@index'
-        ]);
-
-        /**
-         * News
-         */
-        Route::get('/tin-tuc', [
-            'as' => 'news',
-            'uses' => 'NewsController@index'
-        ]);
-
-        /**
-         * Teacher
-         */
-        Route::get('/teacher', [
-            'as' => 'teacher',
-            'uses' => 'TeacherController@index'
-        ]);
-
-        /**
-         * Service
-         */
-        Route::get('/dich-vu', [
-            'as' => 'service',
-            'uses' => 'ServiceController@index'
-        ]);
-
-        /**
-         * Signin
-         */
-        Route::get('/signin', [
-            'as' => 'signin',
-            'uses' => 'SigninController@index'
-        ]);
-
-        /**
-         * Student login
-         */
-        Route::get('/student-login', [
-            'as' => 'student-login',
-            'uses' => 'StudentLoginController@index'
-        ]);
-
-        /**
-         * Student profile
-         */
-        Route::get('/student-profile', [
-            'as' => 'student-profile',
-            'uses' => 'StudentProfileController@index'
-        ]);
-
-
-        /**
-         * Teacher profile
-         */
-        Route::get('/teacher-profile', [
-            'as' => 'teacher-profile',
-            'uses' => 'TeacherProfileController@index'
-        ]);
-
-        /**
-         * Our teacher
-         */
-        Route::get('/our-teacher', [
-            'as' => 'our-teacher',
-            'uses' => 'OurTeacherController@index'
-        ]);
-
-        /**
-         * Course
-         */
-        Route::get('/{category}', [
-            'as' => 'course',
-            'uses' => 'CourseController@index'
-        ])->where(['category' => '(?![^"]*('.$temp.')[^"]*)[a-zA-Z0-9-_]+']);
-
-        /**
-         * Course detail
-         */
-        Route::get('{category}/{post}-{id}', [
-            'as' => 'course-detail',
-            'uses' => 'CourseDetailController@index'
-        ])->where(['category' => '[a-zA-Z0-9-_]+', 'post' => '[a-zA-Z0-9-_]+', 'id' => '[0-9]+']);
 
     });
 });
